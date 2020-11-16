772c8c8e03a445a2d0d7ed7875e6842f8d55c52b
David Poll <david@depoll.com>
2020-11-16 14:49:46 -0800

    Remove main.ts module and add nop module

--- modules/index.ts
+++ modules/index.ts
@@ -1,2 +1,10 @@
 /* This file exports all modules created with 'npm run create-module --name=<name>' */
-export * from './main';
+import { STGenSmartApp } from '@stgen/stgen';
+import { STGenSmartAppModule } from 'lib/module';
+
+// Export a default no-op module so that there is always >=1 module.
+export const NopModule: STGenSmartAppModule = {
+  configure(app: STGenSmartApp): null {
+    return null;
+  },
+};
